ebf4943a6f291c99513f921bed977383bf69ea4d
Robert Yeakel <robertyeakeltroidl@gmail.com>
2024-01-08 10:55:05 -0800

    ch: adds namespace format as per instructions

--- index.ts
+++ index.ts
@@ -1,105 +1,107 @@
-type Board = (string | null)[][];
-type Position = [number, number];
-type Delta = [number, number];
+namespace YeakelRobert {
+    export type Board = (string | null)[][];
+    export type Position = [number, number];
+    export type Delta = [number, number];
 
-interface IKnightMoves {
-    solve(): number;
+    export interface IKnightMoves {
+        solve(): number;
+    }
+
+    export class YeakelRobert implements IKnightMoves {
+        private board: Board;
+        private moveDeltas: Delta[];
+        private restrictedCharacters: Set<string>;
+        private maxMoves: number;
+        private maxRestrictedCharacters: number;
+
+        constructor({
+            board,
+            moveDeltas,
+            restrictedCharacters,
+            maxMoves,
+            maxRestrictedCharacters,
+        }: {
+            board: Board;
+            moveDeltas: Delta[];
+            restrictedCharacters: Set<string>;
+            maxMoves: number;
+            maxRestrictedCharacters: number;
+        }) {
+            this.board = board;
+            this.moveDeltas = moveDeltas;
+            this.restrictedCharacters = restrictedCharacters;
+            this.maxMoves = maxMoves;
+            this.maxRestrictedCharacters = maxRestrictedCharacters;
+        }
+
+        private isValidPosition(position: Position): boolean {
+            const [x, y] = position;
+            const value = this.board[x]?.[y];
+            return !!value;
+        }
+
+        private getNextPosition(position: Position, delta: Delta): Position {
+            return [position[0] + delta[0], position[1] + delta[1]];
+        }
+
+        private isRestrictedCharacter(position: Position): boolean {
+            const [x, y] = position;
+            return this.restrictedCharacters.has(this.board[x][y]!);
+        }
+
+        private knightDfs(position: Position, totalMoves = 0, badCharacterCount = 0): number {
+            if (this.isRestrictedCharacter(position)) {
+                badCharacterCount += 1;
+            }
+            if (badCharacterCount === this.maxRestrictedCharacters) {
+                return 0;
+            }
+            if (totalMoves === this.maxMoves) {
+                return 1;
+            }
+
+            let totalPaths = 0;
+
+            for (const delta of this.moveDeltas) {
+                const nextPosition = this.getNextPosition(position, delta);
+
+                const isValidMove = this.isValidPosition(nextPosition);
+                if (isValidMove) {
+                    totalPaths += this.knightDfs(nextPosition, totalMoves + 1, badCharacterCount);
+                }
+            }
+
+            return totalPaths;
+        }
+
+        private traverseBoard(callback: (position: Position) => void): void {
+            for (let x = 0; x < this.board.length; x++) {
+                const row = this.board[x];
+
+                for (let y = 0; y < row.length; y++) {
+                    const position: Position = [x, y];
+                    callback(position);
+                }
+            }
+        }
+
+        private calculateTotalPaths(position: Position): number {
+            return this.knightDfs(position, 0, 0);
+        }
+
+        public solve(): number {
+            let totalPathsForAllCells = 0;
+            this.traverseBoard((position) => {
+                if (this.isValidPosition(position)) {
+                    totalPathsForAllCells += this.calculateTotalPaths(position);
+                }
+            });
+            return totalPathsForAllCells;
+        }
+    }
 }
 
-class YeakelRobert implements IKnightMoves {
-    private board: Board;
-    private moveDeltas: Delta[];
-    private restrictedCharacters: Set<string>;
-    private maxMoves: number;
-    private maxRestrictedCharacters: number;
-
-    constructor({
-        board,
-        moveDeltas,
-        restrictedCharacters,
-        maxMoves,
-        maxRestrictedCharacters,
-    }: {
-        board: Board;
-        moveDeltas: Delta[];
-        restrictedCharacters: Set<string>;
-        maxMoves: number;
-        maxRestrictedCharacters: number;
-    }) {
-        this.board = board;
-        this.moveDeltas = moveDeltas;
-        this.restrictedCharacters = restrictedCharacters;
-        this.maxMoves = maxMoves;
-        this.maxRestrictedCharacters = maxRestrictedCharacters;
-    }
-
-    private isValidPosition(position: Position): boolean {
-        const [x, y] = position;
-        const value = this.board[x]?.[y];
-        return !!value;
-    }
-
-    private getNextPosition(position: Position, delta: Delta): Position {
-        return [position[0] + delta[0], position[1] + delta[1]];
-    }
-
-    private isRestrictedCharacter(position: Position): boolean {
-        const [x, y] = position;
-        return this.restrictedCharacters.has(this.board[x][y]!);
-    }
-
-    private knightDfs(position: Position, totalMoves = 0, badCharacterCount = 0): number {
-        if (this.isRestrictedCharacter(position)) {
-            badCharacterCount += 1;
-        }
-        if (badCharacterCount === this.maxRestrictedCharacters) {
-            return 0;
-        }
-        if (totalMoves === this.maxMoves) {
-            return 1;
-        }
-
-        let totalPaths = 0;
-
-        for (const delta of this.moveDeltas) {
-            const nextPosition = this.getNextPosition(position, delta);
-
-            const isValidMove = this.isValidPosition(nextPosition);
-            if (isValidMove) {
-                totalPaths += this.knightDfs(nextPosition, totalMoves + 1, badCharacterCount);
-            }
-        }
-
-        return totalPaths;
-    }
-
-    private traverseBoard(callback: (position: Position) => void): void {
-        for (let x = 0; x < this.board.length; x++) {
-            const row = this.board[x];
-
-            for (let y = 0; y < row.length; y++) {
-                const position: Position = [x, y];
-                callback(position);
-            }
-        }
-    }
-
-    private calculateTotalPaths(position: Position): number {
-        return this.knightDfs(position, 0, 0);
-    }
-
-    public solve(): number {
-        let totalPathsForAllCells = 0;
-        this.traverseBoard((position) => {
-            if (this.isValidPosition(position)) {
-                totalPathsForAllCells += this.calculateTotalPaths(position);
-            }
-        });
-        return totalPathsForAllCells;
-    }
-}
-
-const KNIGHT_MOVE_DELTAS: Delta[] = [
+const KNIGHT_MOVE_DELTAS: YeakelRobert.Delta[] = [
     [-2, -1],
     [-2, 1],
     [-1, -2],
@@ -112,7 +114,7 @@ const KNIGHT_MOVE_DELTAS: Delta[] = [
 
 const VOWELS = new Set(["a", "e", "i", "o", "u", "y"]);
 
-const board: Board = [
+const board: YeakelRobert.Board = [
     ["a", "b", "c", null, "e"],
     [null, "g", "h", "i", "j"],
     ["k", "l", null, "n", "o"],
@@ -120,6 +122,12 @@ const board: Board = [
     ["u", "v", null, null, "y"],
 ];
 
-const knightTraversal = new YeakelRobert({ board, moveDeltas: KNIGHT_MOVE_DELTAS, restrictedCharacters: VOWELS, maxMoves: 9, maxRestrictedCharacters: 3 });
+const knightTraversal = new YeakelRobert.YeakelRobert({
+    board,
+    moveDeltas: KNIGHT_MOVE_DELTAS,
+    restrictedCharacters: VOWELS,
+    maxMoves: 9,
+    maxRestrictedCharacters: 3,
+});
 const totalPaths = knightTraversal.solve();
 console.log(totalPaths);
